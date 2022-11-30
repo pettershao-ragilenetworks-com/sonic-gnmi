211ad988ec00417e1d1d471f6c6364a31065f4b2
pettershao-ragilenetworks <81281940+pettershao-ragilenetworks@users.noreply.github.com>
2022-11-30 09:47:49 +0800

    Update telemetry.go

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -135,7 +135,7 @@ func main() {
 	}
 
 	opts = []grpc.ServerOption{grpc.Creds(credentials.NewTLS(tlsCfg))}
-	cfg := &gnmi.Config{}
+	cfg = &gnmi.Config{}
 	cfg.Port = int64(*port)
 	cfg.UserAuth = userAuth
 	cfg.EnableTranslibWrite = bool(*gnmi_translib_write)
